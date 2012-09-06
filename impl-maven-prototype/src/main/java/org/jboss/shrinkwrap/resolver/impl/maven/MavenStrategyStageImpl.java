6da313a38ddd9a50f6e0cd5b6404027bf81a7494
Karel Piwko <kpiwko@redhat.com>
2012-09-06 11:46:08 +0200

    SHRINKRES-39 Improved exception message NoResolvedResultException

--- impl-maven-prototype/src/main/java/org/jboss/shrinkwrap/resolver/impl/maven/MavenStrategyStageImpl.java
+++ impl-maven-prototype/src/main/java/org/jboss/shrinkwrap/resolver/impl/maven/MavenStrategyStageImpl.java
@@ -43,11 +43,17 @@ import org.jboss.shrinkwrap.resolver.impl.maven.strategy.TransitiveStrategy;
 import org.jboss.shrinkwrap.resolver.impl.maven.util.Validate;
 import org.sonatype.aether.artifact.Artifact;
 import org.sonatype.aether.collection.CollectRequest;
+import org.sonatype.aether.collection.CollectResult;
 import org.sonatype.aether.collection.DependencyCollectionException;
+import org.sonatype.aether.graph.Dependency;
+import org.sonatype.aether.graph.DependencyNode;
 import org.sonatype.aether.repository.RemoteRepository;
 import org.sonatype.aether.resolution.ArtifactResolutionException;
 import org.sonatype.aether.resolution.ArtifactResult;
+import org.sonatype.aether.resolution.DependencyRequest;
 import org.sonatype.aether.resolution.DependencyResolutionException;
+import org.sonatype.aether.resolution.DependencyResult;
+import org.sonatype.aether.transfer.ArtifactNotFoundException;
 
 /**
  * Implementation of {@link MavenStrategyStage}
@@ -102,30 +108,29 @@ public class MavenStrategyStageImpl implements MavenStrategyStage, MavenWorkingS
 
         // create a copy
         final List<MavenDependency> prefilteredDependencies = preFilter(
-            configureFilterFromSession(session, strategy.getPreResolutionFilter()), session.getDependencies());
+                configureFilterFromSession(session, strategy.getPreResolutionFilter()), session.getDependencies());
         final List<MavenDependency> prefilteredDepsList = new ArrayList<MavenDependency>(prefilteredDependencies);
         final List<MavenDependency> depManagement = new ArrayList<MavenDependency>(session.getDependencyManagement());
 
         final List<RemoteRepository> repos = session.getRemoteRepositories();
         final CollectRequest request = new CollectRequest(MavenConverter.asDependencies(prefilteredDepsList),
-            MavenConverter.asDependencies(depManagement), repos);
+                MavenConverter.asDependencies(depManagement), repos);
 
         // wrap artifact files to archives
         Collection<ArtifactResult> artifactResults = null;
         try {
             // resolution filtering
-            artifactResults = session.execute(request,
-                configureFilterFromSession(session, strategy.getResolutionFilter()));
+            artifactResults = session.execute(request, configureFilterFromSession(session, strategy.getResolutionFilter()));
         } catch (DependencyResolutionException e) {
             Throwable cause = e.getCause();
             if (cause != null) {
                 if (cause instanceof ArtifactResolutionException) {
-                    throw new NoResolvedResultException("Unable to get artifact from the repository");
+                    throw AetherExceptionTransformer.INSTANCE.transform((ArtifactResolutionException) cause);
                 } else if (cause instanceof DependencyCollectionException) {
-                    throw new NoResolvedResultException("Unable to collect dependency tree for given dependencies");
+                    throw AetherExceptionTransformer.INSTANCE.transform((DependencyCollectionException) cause);
                 }
-                throw new NoResolvedResultException("Unable to collect/resolve dependency tree for a resulution");
             }
+            throw AetherExceptionTransformer.INSTANCE.transform(e);
         }
 
         // TODO Do we need to do real post-resolution filtering? Seems excessive, plus it breaks tests
@@ -143,8 +148,8 @@ public class MavenStrategyStageImpl implements MavenStrategyStage, MavenWorkingS
 
         for (final Artifact artifact : artifactsToFilter) {
             final MavenCoordinate coordinate = MavenCoordinates.createCoordinate(artifact.getGroupId(),
-                artifact.getArtifactId(), artifact.getBaseVersion(),
-                PackagingType.fromPackagingType(artifact.getExtension()), artifact.getClassifier());
+                    artifact.getArtifactId(), artifact.getBaseVersion(),
+                    PackagingType.fromPackagingType(artifact.getExtension()), artifact.getClassifier());
             final MavenDependency dependency = MavenDependencies.createDependency(coordinate, ScopeType.COMPILE, false);
             if (postResolutionFilter.accepts(dependency)) {
                 filteredArtifacts.add(artifact);
@@ -171,7 +176,7 @@ public class MavenStrategyStageImpl implements MavenStrategyStage, MavenWorkingS
             if (PackagingType.POM.equals(coordinate.getPackaging())) {
                 if (log.isLoggable(Level.FINER)) {
                     log.finer("Filtering out POM dependency resolution: " + coordinate
-                        + "; its transitive dependencies will be included");
+                            + "; its transitive dependencies will be included");
                 }
 
                 return false;
@@ -185,15 +190,14 @@ public class MavenStrategyStageImpl implements MavenStrategyStage, MavenWorkingS
         }
 
         @Override
-        public MavenResolutionFilterInternalView setDefinedDependencyManagement(
-            final List<MavenDependency> dependencyManagement) {
+        public MavenResolutionFilterInternalView setDefinedDependencyManagement(final List<MavenDependency> dependencyManagement) {
             return this;
         }
 
     }
 
     private MavenResolutionFilter configureFilterFromSession(final MavenWorkingSession session,
-        final MavenResolutionFilter filter) {
+            final MavenResolutionFilter filter) {
 
         Validate.notNull(session, "MavenWorkingSession must not be null");
         // filter can be null
@@ -203,9 +207,8 @@ public class MavenStrategyStageImpl implements MavenStrategyStage, MavenWorkingS
 
         // Represent as our internal SPI type
         assert filter instanceof MavenResolutionFilterInternalView : "All filters must conform to the internal SPI: "
-            + MavenResolutionFilterInternalView.class.getName();
-        final MavenResolutionFilterInternalView internalFilterView = MavenResolutionFilterInternalView.class
-            .cast(filter);
+                + MavenResolutionFilterInternalView.class.getName();
+        final MavenResolutionFilterInternalView internalFilterView = MavenResolutionFilterInternalView.class.cast(filter);
 
         // prepare dependencies
         List<MavenDependency> dependencies = session.getDependencies();
@@ -269,5 +272,96 @@ public class MavenStrategyStageImpl implements MavenStrategyStage, MavenWorkingS
             this.session.disableMavenCentral();
         }
         return this;
+    }
+
+    /**
+     * An utility to wrap an Aether resolution Exception into NoResolutionException
+     *
+     * @author <a href="mailto:kpiwko@redhat.com">Karel Piwko</a>
+     *
+     */
+    private static enum AetherExceptionTransformer {
+        INSTANCE;
+
+        public NoResolvedResultException transform(DependencyCollectionException cause) {
+            StringBuilder sb = new StringBuilder(
+                    "Unable to get artifact(s) from the repository: Failed to collect dependencies for: ");
+            CollectResult result = cause.getResult();
+            if (result != null) {
+                CollectRequest request = result.getRequest();
+                Dependency root = request.getRoot();
+                if (root != null) {
+                    sb.append(root);
+                } else {
+                    sb.append(request.getDependencies());
+                }
+            }
+            if (result != null && !result.getExceptions().isEmpty()) {
+                sb.append(", due to ").append(result.getExceptions().get(0).getMessage());
+            }
+
+            return new NoResolvedResultException(sb.toString());
+
+        }
+
+        public NoResolvedResultException transform(ArtifactResolutionException cause) {
+            StringBuilder sb = new StringBuilder(
+                    "Unable to get artifact(s) from the repository: Failed to resolve dependencies: ");
+            // add all missing resolution artifacts
+            for (ArtifactResult result : cause.getResults()) {
+                if (!result.isResolved()) {
+                    sb.append(result.getRequest().getArtifact()).append(", ");
+                }
+
+            }
+            int lastDelimiter = sb.lastIndexOf(", ");
+            if (lastDelimiter == sb.length() - 2) {
+                sb.delete(lastDelimiter, sb.length());
+            }
+
+            // create a detailed message
+            sb.append("\n").append("Detailed message: ");
+            for (ArtifactResult result : ((ArtifactResolutionException) cause).getResults()) {
+                if (!result.isResolved()) {
+                    sb.append(result.getRequest().getArtifact()).append(":\n");
+                    for (Exception detailedCause : result.getExceptions()) {
+                        if (detailedCause instanceof ArtifactNotFoundException) {
+                            sb.append("\t").append(((ArtifactNotFoundException) detailedCause).getMessage()).append("\n");
+                        }
+                    }
+                }
+            }
+
+            return new NoResolvedResultException(sb.toString());
+        }
+
+        public NoResolvedResultException transform(DependencyResolutionException cause) {
+            StringBuilder sb = new StringBuilder(
+                    "Unable to get artifact(s) from the repository: Failed to resolve dependencies for: ");
+            DependencyResult result = cause.getResult();
+            if (result != null) {
+                DependencyRequest request = result.getRequest();
+                DependencyNode root = request.getRoot();
+                if (root != null) {
+                    sb.append(root);
+                }
+            }
+
+            // create a detailed message
+            sb.append("\n").append("Detailed message: ");
+            for (ArtifactResult artifactResult : result.getArtifactResults()) {
+                if (!artifactResult.isResolved()) {
+                    sb.append(artifactResult.getRequest().getArtifact()).append(":\n");
+                    for (Exception detailedCause : artifactResult.getExceptions()) {
+                        if (detailedCause instanceof ArtifactNotFoundException) {
+                            sb.append("\t").append(((ArtifactNotFoundException) detailedCause).getMessage()).append("\n");
+                        }
+                    }
+                }
+            }
+
+            return new NoResolvedResultException(sb.toString());
+
+        }
     }
 }
